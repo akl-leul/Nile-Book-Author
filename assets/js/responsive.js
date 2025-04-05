6fd2ad9353c7203728f5cb659c6151afb8b5fc64
Leul Ayfokru <abateisking@gmail.com>
2025-04-05 14:31:58 +0000

    HOme page

--- assets/js/responsive.js
+++ assets/js/responsive.js
@@ -1,1 +1,12 @@
 /*main js for responsiveness*/
+
+function menu(){
+    document.getElementById("phone").style.display = "flex";
+    document.getElementById("phone").style.animation = "fadeIn .5s"; 
+}
+
+function menuclose(){
+    document.getElementById("phone").style.display = "none";
+    
+    document.getElementById("phone").style.animation = "fadeOut 1s"; 
+}
